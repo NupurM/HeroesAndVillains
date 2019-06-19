68e3a91258d12afffd26ba40b4d2085899a18d55
Nupur M <NupurM@live.com>
2019-06-19 09:55:21 -0700

    Add styling to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Character, Profession } from '../characters/character';
 import { CharacterService } from '../characters/character.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
